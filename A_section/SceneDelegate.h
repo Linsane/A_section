62ac66081da3e627e3908fe117e1f7a5119c804d
linxiangyu2 <linxiangyu2@yy.com>
2020-11-14 23:20:25 +0800

    Initial Commit

---
+++ A_section/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  A_section
+//
+//  Created by Sean on 2020/11/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ A_section/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  A_section
+//
+//  Created by Sean on 2020/11/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
